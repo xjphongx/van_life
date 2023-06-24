4bca77258f5c1351746a069488efde83a25a2085
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-06-23 23:20:13 -0700

    adding files to style in css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return(
+    <>
+      <footer> Copyright 2022 #VANLIFE</footer>
+    </>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import About from './routes/About';
 
 
@@ -10,6 +11,13 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <main className='main-container'>
+        <h1>You got the travbel plans, we got the travel vans.</h1>
+        <h3>Add adventure to your life by joinging the #vanlife movment. &#10; Rent the perfect van to make your perfect road trip.</h3>
+        <button>Find your van</button>
+      
+      </main> 
+      <Footer></Footer>
     </>
   )
 }
