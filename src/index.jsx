ea7d77ff63be2d334bd152bdb3492bb9b95d46b8
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-06-23 19:12:17 -0700

    added google fonts

--- src/index.jsx
+++ src/index.jsx
@@ -2,43 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-import About from './components/About';
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+import Navbar from './components/Navbar';
+import About from './routes/About';
+
 
 function App() {
   return (
-    <h1>Start here</h1>
+    <>
+      <Navbar></Navbar>
+    </>
   )
 }
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <BrowserRouter>
-      <nav>
-        <Link to="/about">About</Link>
-        <Link>Vans</Link>
-      </nav>
-      
-      
+    <BrowserRouter> 
       <Routes>
         <Route path="/" element={<App />}/>
         <Route path="/about" element={<About/>}/>
-        
       </Routes>
     </BrowserRouter>
     
