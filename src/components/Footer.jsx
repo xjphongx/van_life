c70acbd28b26e29181df69b4ecc344c54571f040
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-06-25 19:27:48 -0700

    added inline css for practice

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 export default function Footer() {
+  const styles = {
+    backgroundColor: '#252525',
+    color:'#AAAAAA',
+    paddingTop:25,
+    paddingBottom:25,
+    paddingLeft:70,
+    paddingRight:70,
+    textAlign: 'center',
+    fontFamily: 'Inter', 
+  }
   return(
-    <>
-      <footer> Copyright 2022 #VANLIFE</footer>
-    </>
+      <footer style={styles} > Copyright 2022 #VANLIFE</footer>
   )
 }
