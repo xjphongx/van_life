3ee3b2453ce1ff4c6e89b634ef9d164afa941315
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-07-05 19:49:57 -0700

    fixed some functions

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 
 export default function Home(){
@@ -8,7 +9,7 @@ export default function Home(){
         <div className='content-container'>
           <h1>You got the travel plans, we got the travel vans.</h1>
           <h3>Add adventure to your life by joinging the #vanlife movment. &#10; Rent the perfect van to make your perfect road trip.</h3>
-          <button className='find-van-button'>Find your van</button>
+          <Link to='/vans' className='find-van-button'>Find your van</Link>
         </div>
       </main> 
       
