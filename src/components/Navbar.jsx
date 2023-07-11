86b9218e70d402d0522ae638819a8033942416f8
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-07-10 21:10:23 -0700

    added host button in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ export default function Navbar(){
     <nav className="nav-bar-container">
       <Link to="/" className="vanlife-header">#VANLIFE</Link>
       <div className="route-container">
+        <Link to='/host' className="nav-link-route">Host</Link>
         <Link to="/about" className="nav-link-route">About</Link>
         <Link to="/vans" className="nav-link-route">Vans</Link>
       </div>
