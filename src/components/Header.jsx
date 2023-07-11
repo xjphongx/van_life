1b1756ce6bc091f4db0b5dc668a32646aa202a36
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-07-10 19:26:16 -0700

    setting up new files for layout route

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+
+export default function Header(){
+  return(<>
+    <header className="header-section">
+      <Navbar/>
+    </header>
+  </>)
+}
