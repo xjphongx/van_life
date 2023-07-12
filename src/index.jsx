90ff83f529408109438066b8d75801eb497a3a6a
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-07-11 22:43:26 -0700

    finished with layout routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from './routes/Home';
 import Layout from './components/Layout';
@@ -38,15 +38,8 @@ function App() {
             </Route> 
           </Route>
 
-          
-
-
-
-
         </Routes>      
     </BrowserRouter>
-    
-      
   )
 }
 
