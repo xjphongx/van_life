0fd35295249174fc0a7c60090993f06a6dc541eb
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-07-19 21:09:36 -0700

    made the filter list more interactive by allowing the selected button to remain the same color

--- src/routes/Vans/Vans.jsx
+++ src/routes/Vans/Vans.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import {Link, useSearchParams} from "react-router-dom"
 
 export default function Vans(){
   const [vans, setVans] = React.useState([])
   
+  //gets the query parameter from URL(/localhost/vans?type=Simple)
+  const [searchParams, setSearchParams] = useSearchParams()
+  const typeFilter = searchParams.get("type") //what the function will filter out based upone van type
+
+
+
   //fetch data when the van page loads
   React.useEffect(()=>{
     //helper function to get van data from mirage JS API
@@ -16,21 +22,27 @@ export default function Vans(){
   },[])
   
  
+  //Filter Feature using useSearchParams()
+  //filter the list if there is a query parameter of type={"Simple,Rugged,Luxury"}
+  //Use a ternary operator to see if typeFilter exists? chooose van to filter after
+  const filterVansList =typeFilter
+    ? vans.filter(van=> van.type ===typeFilter) 
+    : vans 
 
-  const vansElement = vans.map(van=>{
+  const vansElement = filterVansList.map(van=>{
     //captialize the first character of the type
     //ex: given "simple" div will output Simple
     /* const captialWord = van.type.charAt(0).toUpperCase() + van.type.slice(1) */
 
     return (
       <div key={van.id} className="van-tile">
-        <Link to={`/vans/${van.id}`}>
+        <Link className="van-link" to={`/vans/${van.id}`}>
           <img className="van-image" src={van.imageUrl}/>
           <div className="van-info">
             <h3>{van.name}</h3>
             <p>${van.price} <span>/day</span></p>
           </div>
-          <p className={`type-tag-${van.type}`} >{van.type}</p>
+          <p className={`van-type ${van.type} selected` } >{van.type}</p>
         </Link>
           
       </div>
@@ -43,10 +55,19 @@ export default function Vans(){
       <div className='option-container'>
         <h1>Explore our van options</h1>
         <div className='filter-van-container'>
-          <button className='filter-button'>Simple</button>
-          <button className='filter-button'>Luxury</button>
-          <button className='filter-button'>Rugged</button>
-          <button className='clear-filter-button'>Clear filters</button>
+          {/* setSearchParams: look for object with type Simple */}
+          <button onClick={()=>setSearchParams({type: "Simple"})} 
+            className={`van-type Simple ${typeFilter==="Simple"?"selected":""}`}
+            >Simple</button> 
+          <button onClick={()=>setSearchParams({type: "Luxury"})} 
+            className={`van-type Luxury ${typeFilter==="Luxury"?"selected":""}`}
+            >Luxury</button>
+          <button onClick={()=>setSearchParams({type: "Rugged"})} 
+            className={`van-type Rugged ${typeFilter==="Rugged"?"selected":""}`}
+            >Rugged</button>
+          {console.log(typeFilter)}
+          {typeFilter&&<button onClick={()=>setSearchParams({})} className='clear-filter-button'>Clear filters</button>}
+          
         </div>
         
       </div>
@@ -57,3 +78,9 @@ export default function Vans(){
     </div>
   )
 }
+
+
+/* <Link to="?type=Simple" className='filter-button'>Simple</Link>
+          <Link to="?type=Luxury" className='filter-button'>Luxury</Link>
+          <Link to="?type=Rugged" className='filter-button'>Rugged</Link>
+          <Link to="." className='clear-filter-button'>Clear filters</Link> */
