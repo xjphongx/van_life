0627514b6d10a6e70a5d0dc27f17f4ff6934d444
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-07-20 15:46:51 -0700

    fixed css bug

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout(){
   return(
     <>
       <Header/>
-      <main>
+      <main className="main-section">
         <Outlet/> {/* Used to render the desire child path */}
       </main>
       <Footer/>
