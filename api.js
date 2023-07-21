046721fea04b2534837b85f82cdc3cf90416546f
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-07-20 23:37:04 -0700

    worked around this bug

--- api.js
+++ api.js
@@ -1,6 +1,17 @@
 /* Create a separate file to handle fetch request and export and import to desired file */
 export async function getVans(){
-  const response = await fetch("/api/vans")
-  const data = await response.json()
+  const res = await fetch("/api/vans")
+  console.log(res)
+  //check if fetch request has an error
+  if(!res.ok){ //NOTE: change this to !res.ok
+    throw{
+      //throw an error if response is NOT ok 
+      message: "failed to fetch vans",
+      statusText: res.statusText,
+      status:res.status
+    }
+  }
+  const data = await res.json()
+  console.log(data)
   return data.vans
 }
