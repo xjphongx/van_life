50ca4cbf1f1fef8e7d9c14e4c847770c7dd9e591
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-07-30 23:57:56 -0700

    added basic RESTful API

--- src/server/routes/vans.mjs
+++ src/server/routes/vans.mjs
@@ -1,15 +1,22 @@
 import express from "express";
 import db from "../db/conn.mjs";
+import van from '../model/van.mjs'
 
 const router = express.Router();
 
 //get a list of all the records of vans
 router.get("/", async (req, res)=>{
-  res.send("van list")
-  const vanCollection = await db.collection("vans")
+  
+  //res.send("van list")
+  try {
+    const vans = await van.find()
+    return res.json(vans)
+  } catch(err){
+    res.status(500).json({message: err.message})
+  }
+  /* const vanCollection = await db.collection("vans")
   const results = await vanCollection.find({}).toArray()
-  console.log(results)
-  res.send(results).status(200)
+  res.send(results).status(200) */
 })
 
 export default router;
--- src/server/server.mjs
+++ src/server/server.mjs
@@ -1,14 +1,26 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs"
+import db from "./db/conn.mjs";
 import users from "./routes/users.mjs";
-import vans from "./routes/vans.mjs"
+import  vans from "./routes/vans.mjs"
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
+
+/* {
+  origin: 'http://localhost:5173/', // use your actual domain name (or localhost), using * is not recommended
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  credentials: true
+} */
 app.use(express.json());
+
+db.on('error', (error) => console.error(error))
+db.once('open', ()=>{console.log('connected to database')})
+
 
 app.use("/users", users) //link the users route to a /users path;
 app.use("/vans", vans)
